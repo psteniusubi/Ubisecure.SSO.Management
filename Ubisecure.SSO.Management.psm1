2a2235dbc5f475f69a6e3a718d071ff222b0dc66
petteri.stenius@ubisecure.com <petteri.stenius@ubisecure.com>
2021-06-01 19:20:45 +0300

    updates

--- Ubisecure.SSO.Management.psm1
+++ Ubisecure.SSO.Management.psm1
@@ -2,14 +2,19 @@
 function ConvertTo-Form {
     [CmdletBinding()]
     Param(
-        [Parameter(Position=0,ValueFromPipeline=$true)] [hashtable] $Map,
-        [Parameter()] [switch] $Child
+        [Parameter(Position = 0, ValueFromPipeline = $true)] 
+        [hashtable] 
+        $Map,
+
+        [Parameter()] 
+        [switch] 
+        $Child
     )
     # Enabled, Type, Name, Attributes
     Process {
         $local:form = New-QueryString
         [bool]$Map["Enabled"] | ? { $_ } | % { $local:form = $local:form | Add-QueryString "enabled" "true" }
-        if($Child) {
+        if ($Child) {
             [string]$Map["ChildType"] | ? { $_ } | % { $local:form = $local:form | Add-QueryString "type" $_ }
             [string]$Map["ChildName"] | ? { $_ } | % { $local:form = $local:form | Add-QueryString "name" $_ }
         }
@@ -23,7 +28,9 @@ function ConvertTo-Form {
 function ConvertFrom-Map {
     [CmdletBinding()]
     Param(
-        [Parameter(Position=0,ValueFromPipeline=$true)] [hashtable] $InputObject
+        [Parameter(Position = 0, ValueFromPipeline = $true)]
+        [hashtable] 
+        $InputObject
     )
     Begin {
         Get-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
@@ -31,9 +38,9 @@ function ConvertFrom-Map {
     Process {
         $InputObject | % {
             $local:out = "@{"
-            $local:out += $(foreach($local:key in $_.Keys) {
-                "$($local:key)=$($_[$local:key])"
-            }) -join ";" 
+            $local:out += $(foreach ($local:key in $_.Keys) {
+                    "$($local:key)=$($_[$local:key])"
+                }) -join ";" 
             $local:out += "}"
             $local:out
         }
@@ -43,8 +50,13 @@ function ConvertFrom-Map {
 function Copy-Map {
     [CmdletBinding()]
     Param(
-        [Parameter()] [hashtable] $Map,
-        [Parameter()] [string[]] $Keys
+        [Parameter()] 
+        [hashtable] 
+        $Map,
+        
+        [Parameter()] 
+        [string[]] 
+        $Keys
     )
     Begin {
         Get-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
@@ -58,44 +70,75 @@ function Copy-Map {
 }
 
 function Invoke-Api {
-    [CmdletBinding(SupportsShouldProcess=$true)]
+    [CmdletBinding(SupportsShouldProcess = $true)]
     Param(
-        [Parameter()] [Microsoft.PowerShell.Commands.WebRequestMethod] $Method = [Microsoft.PowerShell.Commands.WebRequestMethod]::Get,
-        [Parameter(Position=0,ValueFromPipeline=$true)] [PSTypeName("SSO.ObjectPath")] $InputObject,
-        [Parameter()] [hashtable] $Headers = $null,
-        [Parameter()] [AllowNull()] [string] $ContentType = "application/x-www-form-urlencoded",
-        [Parameter()] [AllowNull()] [string] $Accept = $null,
-        [Parameter()] [AllowNull()] [hashtable] $Query = $null,
-        [Parameter()] [AllowNull()] [object] $Body = $null,
-        [Parameter()] [PSTypeName("Context")] $Context = (GetContext)
+        [Parameter()] 
+        [Microsoft.PowerShell.Commands.WebRequestMethod] 
+        $Method = [Microsoft.PowerShell.Commands.WebRequestMethod]::Get,
+        
+        [Parameter(Position = 0, ValueFromPipeline = $true)] 
+        [PSTypeName("SSO.ObjectPath")] 
+        $InputObject,
+        
+        [Parameter()] 
+        [hashtable] 
+        $Headers = $null,
+
+        [Parameter()] 
+        [AllowNull()] 
+        [string] 
+        $ContentType = "application/x-www-form-urlencoded",
+        
+        [Parameter()] 
+        [AllowNull()] 
+        [string] 
+        $Accept = $null,
+        
+        [Parameter()] 
+        [AllowNull()] 
+        [hashtable] 
+        $Query = $null,
+        
+        [Parameter()] 
+        [AllowNull()] 
+        [object] 
+        $Body = $null,
+
+        [Parameter()] 
+        [PSTypeName("Context")] 
+        $Context = (GetContext)
     )
     Begin {
         Get-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
         $local:h = $Context.Bearer | ConvertTo-OAuthHttpBearer | ConvertTo-OAuthHttpAuthorization
-        if($Accept) {
-            $local:h += @{"Accept"=$Accept}
+        if ($Accept) {
+            $local:h += @{"Accept" = $Accept }
         }
-        if($Headers) {
+        if ($Headers) {
             $local:h += $Headers
         }
         $local:q = $null
-        if($Query -and ($Query.Count -gt 0)) {
+        if ($Query -and ($Query.Count -gt 0)) {
             $local:q = New-QueryString | Add-QueryString -Values $Query | ConvertTo-QueryString
         }
-        if($Body -is [string]) {
+        if ($Body -is [string]) {
             $Body = [System.Text.Encoding]::UTF8.GetBytes($Body)
         }
     }
     Process {
         $InputObject | % { "$($Context.BaseUri)$($_.ToString())" } | % {
             $local:u = $_
-            if($local:q) {
+            if ($local:q) {
                 $local:u += "?"
                 $local:u += $local:q
             }
             Write-Verbose "$Method $($local:u) Content-Type:$ContentType Body:$Body" 
-            if(($Method -eq "Get") -or $PSCmdlet.ShouldProcess($local:u, $Method)) {
-                Invoke-RestMethod -Method $Method -Uri $local:u -ContentType $ContentType -Headers $local:h -Body $Body -UseBasicParsing
+            if (($Method -eq "Get") -or $PSCmdlet.ShouldProcess($local:u, $Method)) {
+                try {
+                    Invoke-RestMethod -Method $Method -Uri $local:u -ContentType $ContentType -Headers $local:h -Body $Body -UseBasicParsing
+                } catch {
+                    $PSCmdlet.WriteError($_)
+                }
             }
         }
     }
